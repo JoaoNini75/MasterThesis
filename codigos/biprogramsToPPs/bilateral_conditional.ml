883e4e61be66786d07ec764c913419ca8e400717
João Nini <jfnini@sapo.pt>
2025-04-17 15:15:50 +0100

    Added biprograms with specifications

--- codigos/biprogramsToPPs/bilateral_conditional.ml
+++ codigos/biprogramsToPPs/bilateral_conditional.ml
@@ -2,9 +2,9 @@
     not (a = b) *)
 
 let pp (c_l : bool) (c_r : bool) : int * int =
-    assert ( bool_diff c_l c_r );
+    assert ( c_l = c_r );
     if c_l then (1, 1)
     else (0, 0)
-(*@ requires bool_diff c_l c_r
+(*@ requires c_l = c_r
     ensures match result with (l_res, r_res) -> l_res = r_res *)
   
--- codigos/biprogramsToPPs/induc_var_strength_red.ml
+++ codigos/biprogramsToPPs/induc_var_strength_red.ml
@@ -3,16 +3,15 @@ let pp (b_l : int) (b_r : int) (c_l : int) (c_r : int)
 
     let i_l = ref 0 in
     let i_r = ref 0 in
-    let j_l = ref c_l in
+    let j_l = ref 0 in
     let j_r = ref c_r in
     let x_l = ref 0 in
     let x_r = ref 0 in          
 
     while (!i_l < n_l) do
         (*@ invariant !i_l >= 0 && !i_r >= 0 && !i_l = !i_r && (n_l >= 0 -> !i_l <= n_l)
-            invariant !j_l = !i_l * b_l + c_l
             invariant !j_r = !i_r * b_r + c_r
-            invariant !j_l = !j_r && !x_l = !x_r 
+            invariant !x_l = !x_r 
             variant n_l - !i_l *)
         j_l := !i_l * b_l + c_l;
         x_r := !x_r + !j_r;
