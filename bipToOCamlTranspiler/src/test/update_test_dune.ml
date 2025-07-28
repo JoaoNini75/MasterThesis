c46218be4f626cb7547af4e63a65b2c1c3821864
João Nini <jfnini@sapo.pt>
2025-07-28 18:07:54 +0100

    Wrote script to update test/dune

---
+++ bipToOCamlTranspiler/src/test/update_test_dune.ml
@@ -0,0 +1,78 @@
+open Sys
+open Array
+open List
+
+let header_text = {|;; test/dune
+
+;; For each test, we declare a rule that:
+;;  1. Depends on the .bip input and the .expected output
+;;  2. Runs biplang.exe on the .bip, capturing stdout to .actual
+;;  3. Diffs .expected vs .actual under the @runtest alias
+
+;; If they differ, the rule fails;
+;; if they're the same, the rule succeeds quietly.
+
+|}
+
+let rule_base_text : (string -> string -> string -> string -> string     
+  -> string -> string, unit, string) format = 
+{|
+(rule
+ (alias runtest)
+ (deps
+  %s.bip
+  %s.expected)
+ (action
+		(progn
+	 	(with-stdout-to %s.actual
+				(run dune exec .././biplang.exe %s.bip))
+	 	(run diff %s.expected %s.actual))))
+|}
+
+let write_string_to_file ~filename (s : string) =
+  let oc = open_out filename in
+  output_string oc s;
+  close_out oc
+
+let () =
+	let exclude_files = 
+		[	"dune";
+		 	"informal_tests.bip";
+			"update_test_dune.ml"] in
+
+  let dir_files = readdir "../test" in
+
+  for i = 0 to Array.length dir_files - 1 do
+		let filename = get dir_files i in
+
+		if List.mem filename exclude_files then (
+			(*Printf.printf "Excluded: %s\n" filename;*)
+			set dir_files i ""
+		) else if (String.ends_with ~suffix:".expected" filename) then (
+			set dir_files i "";
+			(*Printf.printf "Expected: %s\n" filename;*)
+		) else (
+			set dir_files i (String.sub filename 0 (String.length filename - 4));
+			(*Printf.printf "Bip: %s\n" filename*)
+		)
+
+  done;
+
+	let final_text = ref header_text in
+	(*Printf.printf "\n\n";*)
+	Array.sort String.compare dir_files;
+
+	for i = 0 to Array.length dir_files - 1 do
+		let filename = get dir_files i in
+		if String.equal filename "" then ()
+		else (
+			(*Printf.printf "%s\n" filename;*) 
+			let rule_text = Printf.sprintf rule_base_text 
+				filename filename filename filename filename filename in
+			final_text := !final_text ^ rule_text
+		)
+	done;
+
+	(*Printf.printf "\n\n%s" !final_text; *)
+	Printf.printf "test/dune updated.\n";
+	write_string_to_file ~filename:"dune" !final_text
