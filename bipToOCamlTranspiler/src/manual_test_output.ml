c670984f15bc26fe1624341558d8dafacd86fa09
João Nini <jfnini@sapo.pt>
2025-09-17 13:24:28 +0100

    Writing translator and printer subsection

--- bipToOCamlTranspiler/src/manual_test_output.ml
+++ bipToOCamlTranspiler/src/manual_test_output.ml
@@ -1,23 +1,23 @@
-let list_pattern_matching (l) =
-  let res1 = (
-    match l with
-    | [] -> "empty"
-    | x :: xs -> "head = x"
-  ) in
-  let res2 = (
-    match l with
-    | fnd :: snd :: tail -> "1 + 2 + tail"
-    | head :: tail -> "head + tail"
-    | _ -> "otherwise"
-  ) in
-  let res3 = (
-    match l with
-    | [] -> "len 0"
-    | [ x ] -> "len 1"
-    | [ x; y ] -> "len 2"
-    | [ x; y; z ] -> "len 3"
-    | _ -> "len 4 or more"
-  ) in
-  res1
-(*@ ensures true *)
+let pow_major (target_l : int) (target_r : int) (multiple_of_l) (multiple_of_r) : int * int =
+  let counter_l = ref (0) in
+  let counter_r = ref (0) in
+
+  while ((!counter_l < target_l) || (!counter_r < target_r)) do
+    (*@ invariant (!counter_l < target_l && mod !counter_l multiple_of_l = 0) || (!counter_r < target_r && mod !counter_r multiple_of_r = 0) || (not (!counter_l < target_l) && not (!counter_r < target_r)) || (!counter_l < target_l && !counter_r < target_r) *)
+    if ((!counter_l < target_l) && ((!counter_l mod multiple_of_l) = 0))
+    then begin 
+      counter_l := (!counter_l + 1)
+    end else begin 
+      if ((!counter_r < target_r) && ((!counter_r mod multiple_of_r) = 0))
+      then begin 
+        counter_r := (!counter_r + 1)
+      end else begin 
+        counter_l := (!counter_l + 1);
+        counter_r := (!counter_r + 1)
+      end
+    end
+  done;
+
+  (!counter_l, !counter_r)
+(*@ *)
 
